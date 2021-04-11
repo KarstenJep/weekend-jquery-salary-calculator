c1beeaddbb4b290dc2c66ff80275478e9b970c94
Karsten Jepsen <Kjepsen86@gmail.com>
2021-04-11 12:30:43 -0500

    Completed Base Mode

--- client.js
+++ client.js
@@ -1,40 +1,43 @@
-let employeeSalary = [];
-let totalMonthly = 0;
+let employeeSalary = []; // array for data storage
+let totalMonthly = 0; // variable for displaying running total of salary inputs
 
-$(document).ready(onReady);
+$(document).ready(onReady); // calling jQuery
 
-function onReady() {
-    console.log('Hi jQuery');
+function onReady() { // setting up button functionality
+    // TEST - console.log('Hi jQuery');
     $('#submit').on('click', addEmployee);
     $('#outEmployee').on('click', '.delete', deleteEmployee);
 }
 
 function addEmployee() {
-    console.log('adding employee');
-
-    let employee = {
+    // TEST - console.log('adding employee');
+    let employee = { // putting employee info into a object
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
         id: $('#id').val(),
         title: $('#title').val(),
         annualSalary: $('#annualSalary').val(),
     };
-    employeeSalary.push(employee);
-    console.log(employee.firstName, employee.id, employee.annualSalary);
-    $('#firstName').val('');
-    $('#lastName').val('');
-    $('#id').val('');
+    employeeSalary.push(employee); // pushing employee object into data storage array
+    // TEST - console.log(employee.firstName, employee.id, employee.annualSalary);
+    $('#firstName').val('');    
+    $('#lastName').val(''); 
+    $('#id').val('');           // Clearing all inputs
     $('#title').val('');
     $('#annualSalary').val('');
-    displayEmployee(employee.firstName, employee.lastName, employee.id, employee.title, employee.annualSalary);
+    displayEmployee(employee.firstName, // Calling DOM display function with employee inputs
+                    employee.lastName, 
+                    employee.id, 
+                    employee.title, 
+                    employee.annualSalary);
 }   
     
 function displayEmployee(firstName, lastName, id, title, annualSalary) {
-    console.log('in displayEmployee', firstName);
-    let outEmployee = $('#outEmployee');
-    outEmployee.empty();
-    for ( let person of employeeSalary){
-        console.log('in for loop');
+    // TEST - console.log('in displayEmployee', firstName);
+    let outEmployee = $('#outEmployee'); // creating variable for calling id
+    outEmployee.empty(); // empty DOM
+    for ( let person of employeeSalary){ // loop to append variable and display on DOM
+        // TEST - console.log('in for loop');
         outEmployee.append(`
                         <tr bordercolor="black">
                             <td>${person.firstName}</td>
@@ -48,22 +51,23 @@ function displayEmployee(firstName, lastName, id, title, annualSalary) {
                         </tr>
         `)
     }
-    addSalary(annualSalary);
+    addSalary(annualSalary); // Calling salary calculator function
 }
 
-function addSalary(annualSalary) {
-    console.log('in addSalary');
-    totalMonthly += Number(annualSalary);
-    let tM = $('#totalMonthly');
-    tM.empty();
-    tM.append(totalMonthly);
-    if (totalMonthly > 20000) {
-        console.log('in if');
-        tM.append(`<p id="red"></p>`)
+function addSalary(annualSalary) { 
+    // TEST - console.log('in addSalary');
+    Number(annualSalary /= 12); // divide annual salaries into monthly
+    totalMonthly += annualSalary; // add monthly employee salaries
+    let tM = $('#totalMonthly'); // creating variable for calling id
+    tM.empty(); // empty DOM
+    tM.append('$', totalMonthly); // Display on DOM
+    if (totalMonthly > 20000) { // Creating red background alert if salaries exceed budget
+        // TEST - console.log('in if');
+        tM.css('background-color', 'red');
     }
 }
 
-function deleteEmployee() {
-    console.log('delete employee');
+function deleteEmployee() { // deletes entry off DOM when button clicked
+    // TEST - console.log('delete employee');
     $(this).closest('tr').remove();
 }
